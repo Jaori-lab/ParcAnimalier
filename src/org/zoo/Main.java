d1149041b2e4bcaa93c873f74488ed965123c736
Utilisateur <doaltoc@free.fr>
2022-07-10 07:42:48 +0200

    First Commit

---
+++ src/org/zoo/Main.java
@@ -0,0 +1,10 @@
+package org.zoo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Stub de la méthode généré automatiquement
+			System.out.println("Ceci est un projet de gestion animalier"); 
+	}
+
+}
